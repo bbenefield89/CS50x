bf7b2b1bcb4b6fb4579c718f4284a1f0255bb7bf
Brandon Benefield <bsquared18@gmail.com>
2017-07-26 17:15:27 +0000

    Push greedy.c

---
+++ pset1/greedy.c
@@ -0,0 +1,16 @@
+Push greedy.c
+Find the least amount of change to receive back from x amount of dollars
+Prompts user for dollar amount
+Returns how many US coins it will take to match that amount
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# Explicit paths specified without -i or -o; assuming --only paths...
+# On branch master
+# Your branch is up-to-date with 'origin/master'.
+#
+# Changes to be committed:
+#	new file:   greedy.c
+#
+# Untracked files:
+#	greedy
+#
